643f690d2423d3ee9e7fcea95b7dd9b773ea2acd
Noah (Pham Tran Khoi Nguyen) <ptknguyen04@gmail.com>
2024-10-24 13:23:38 +0100

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Pham Tran Khoi Nguyen on 24/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
